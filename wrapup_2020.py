ba47d0572dcff4de8c37f4cbdcd7b3c86e96a0dd
Anita Okoh <sleekymeeratura@gmail.com>
2020-12-30 16:35:03 +0100

    Update wrapup_2020.py

--- wrapup_2020.py
+++ wrapup_2020.py
@@ -96,7 +96,8 @@ def get_table_download_link(data):
     in:  dataframe
     out: href string
     """
-    figure = data.to_image(format="png",  engine="kaleido")
+#     figure = data.to_image(format="png",  engine="kaleido")
+    figure = plotly.io.to_image(data,format="png",  engine="kaleido")
 #     figure =  py.get(data)
     b64 = base64.b64encode(figure).decode()  
     href = f'<a href="data:file/image;base64,{b64}" download="My 2020 in a wrap.png">Download chart</a>'
